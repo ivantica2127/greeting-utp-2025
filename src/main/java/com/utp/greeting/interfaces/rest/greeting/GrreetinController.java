018a21942e36c4fe36e222f499e3ff210385b820
ivantica2127 <ivanprogramando@gmail.com>
2025-10-29 20:49:25 -0500

    Mi  primer commit

---
+++ src/main/java/com/utp/greeting/interfaces/rest/greeting/GrreetinController.java
@@ -0,0 +1,18 @@
+package com.utp.greeting.interfaces.rest.greeting;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GrreetinController {
+
+
+    @GetMapping
+    public String greeting(@RequestParam(defaultValue = "World") String name) {
+        return  "Hello " + name + "!";
+    }
+}
